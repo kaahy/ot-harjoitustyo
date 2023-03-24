63353f74c5f66020e5a06406c6cd6a3621670566
kaa <113045348+kaahy@users.noreply.github.com>
2023-03-24 20:27:56 +0200

    Lisätään maksukortti_test.py-tiedostoon testejä (tehtävä 6)

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,28 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    # Tehtävä 6:
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_lataa_rahaa_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+
+    def test_rahaa_ottaessa_saldo_vahenee_oikein_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_rahaa_ottaessa_saldo_ei_vahene_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(2000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahaa_ottaessa_paluuarvo_true_tai_false_kertoo_riittivatko_rahat(self):
+        a = self.maksukortti.ota_rahaa(500)
+        b = self.maksukortti.ota_rahaa(2000)
+        self.assertEqual((a, b), (True, False))
