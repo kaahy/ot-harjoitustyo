6f83eacf84716884705334f98e93d61ad28ba464
kaa <113045348+kaahy@users.noreply.github.com>
2023-04-04 17:22:13 +0300

    Lisätään game_test.py

---
+++ src/tests/game_test.py
@@ -0,0 +1,12 @@
+import unittest
+from game import Game
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
+    def test_opens_pointed_card(self):
+        self.game.point_card(6)
+        self.game.open_card()
+
+        self.assertEqual(str(self.game.state["openCards"][0]), str(6))
