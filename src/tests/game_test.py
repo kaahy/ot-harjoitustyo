f6996d37d90fccaae8a9797c0cdd32ef6e6c65b0
kaa <113045348+kaahy@users.noreply.github.com>
2023-04-17 22:00:45 +0300

    Testin päivitys

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -3,7 +3,7 @@ from game import Game
 
 class TestGame(unittest.TestCase):
     def setUp(self):
-        self.game = Game()
+        self.game = Game(10)
 
     def test_opens_pointed_card(self):
         self.game.point_card(6)
