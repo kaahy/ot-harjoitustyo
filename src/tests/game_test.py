cb1eb5bb4e6fe11ac0d4725ebd9fa1c8dec6fd1f
kaa <113045348+kaahy@users.noreply.github.com>
2023-04-18 21:26:21 +0300

    Lisätty testi

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -10,3 +10,13 @@ class TestGame(unittest.TestCase):
         self.game.open_card()
 
         self.assertEqual(str(self.game.state["openCards"][0]), str(6))
+
+    def test_does_not_open_more_than_two_cards(self):
+        self.game.point_card(1)
+        self.game.open_card()
+        self.game.point_card(2)
+        self.game.open_card()
+        self.game.point_card(3)
+        self.game.open_card()
+
+        self.assertEqual(len(self.game.state["openCards"]), 1)
